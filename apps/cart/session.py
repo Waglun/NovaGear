5fc9dc7394f5edfd0e6b2a8a946101098fdb3c7b
anatoliy <makar01.02@mail.ru>
2026-08-03 23:01:03 +0800

    Реализован механизм работы корзины для авторизированного пользователя. Начал работу с сессиями для неавторизированного пользователя

--- apps/cart/session.py
+++ apps/cart/session.py
@@ -1,7 +1,33 @@
+from NovaGear.apps.catalog.models import Product
+
+
 class SessionCart:
     def __init__(self, request):
         self.session = request.session
         self.cart = self.session.setdefault('cart', {})
 
     def save(self):
-        self.session.modified = True
+        self.session.modified = True # Сообщение для Django что данные в сессии были изменены
+
+    def add(self, product_id, quantity=1):
+        product_id = str(product_id)
+        if product_id is self.cart:
+            self.cart[product_id] += quantity
+        else:
+            self.cart[product_id] = quantity
+
+        self.save()
+
+    def items(self):
+        products = Product.objects.filter(id__in=self.cart.keys(), is_active=True)
+
+        items = []
+        for product in products:
+            quantity = self.cart[str(product.id)]
+            items.append({
+                'product_id': product.id,
+                'quantity': quantity,
+                'subtotal_price': product.price * quantity,
+            })
+
+        return items
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -3,43 +3,46 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
 
 from .models import CartItem, Cart
+from .session import SessionCart
 from ..catalog.models import Product
 
 
 @login_required
 def cart(request):
-    cart, created = Cart.objects.get_or_create(user=request.user)
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user)
 
-    cart_items = cart.items.select_related('product')
-    total_price = cart.total_price
-    total_item = cart.total_item
-    Shipping = 0 if total_price >= 5000 else 1200
+        cart_items = cart.items.select_related('product')
+        total_price = cart.total_price
+        total_item = cart.total_item
+        shipping = 0 if total_price >= 5000 else 1200
 
-    context = {
-        'cart_items': cart_items,
-        'total_price': total_price,
-        'total_item': total_item,
-        'is_empty': not cart_items.exists(),
-        'shipping': Shipping,
-    }
+        context = {
+            'cart_items': cart_items,
+            'total_price': total_price,
+            'total_item': total_item,
+            'is_empty': not cart_items.exists(),
+            'shipping': shipping,
+        }
 
-    return render(request, 'cart.html', context)
+        return render(request, 'cart.html', context)
+    else:
+
 
 
 @login_required
 @require_POST
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id, is_active=True)
-    cart, _ = Cart.objects.get_or_create(user=request.user)
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save(update_fields=["quantity"])
-
-    print(cart.id)
-    print(cart.items.count())
-    print(list(cart.items.values()))
+    if request.user.is_authenticated:
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        if not created:
+            cart_item.quantity += 1
+            cart_item.save(update_fields=["quantity"])
+    else:
+        session_cart = SessionCart(request)
+        session_cart.add(product.id)
 
     return redirect(request.META.get("HTTP_REFERER", "catalog:catalog",))
 
