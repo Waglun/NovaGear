37f015c6061f3484014bb0411c9a99b15959b4e9
anatoliy <makar01.02@mail.ru>
2026-08-09 17:50:57 +0800

    Поправил мелочи в шаблоне главной страницы

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
 
+from apps.catalog.models import Product
+
+
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.filter(is_active=True).select_related('category', 'brand')[:8]
+
+    wishlist_ids = (
+        set(request.user.wishlist.values_list("product_id", flat=True))
+        if request.user.is_authenticated
+        else set()
+    )
+
+    context = {
+        'products': products,
+        'wishlist_ids': wishlist_ids,
+    }
+
+    return render(request, 'home.html', context)
